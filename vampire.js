9436660d192266f2a4a6abdc882dac74e3fb58a2
Brad Ruud <bradruud@gmail.com>
2020-04-09 15:54:27 +0000

    code complete for tests 1-4

--- vampire.js
+++ vampire.js
@@ -9,24 +9,42 @@ class Vampire {
   /** Simple tree methods **/
 
   // Adds the vampire as an offspring of this vampire
-  addOffspring(vampire) {
-
+  addOffspring(offspring) {
+    this.offspring.push(offspring);
+    offspring.creator = this;
   }
 
   // Returns the total number of vampires created by that vampire
   get numberOfOffspring() {
+    return this.offspring.length;
 
   }
 
   // Returns the number of vampires away from the original vampire this vampire is
   get numberOfVampiresFromOriginal() {
-
+    let numberOfVampires = 0;
+    let currentVampire = this;
+    while (currentVampire.creator) {
+      currentVampire = currentVampire.creator;
+      numberOfVampires++;
+    }
+    return numberOfVampires;
   }
 
   // Returns true if this vampire is more senior than the other vampire. (Who is closer to the original vampire)
   isMoreSeniorThan(vampire) {
 
-  }
+    if (this === vampire.creator) {
+      return true;
+      } 
+      if (this.name === 'root') {
+        console.log(this.name, Original.name)
+        return true;
+      }
+      if (vampire.creator !== Original) {
+        return false;
+      }
+  };
 
   /** Stretch **/
 
@@ -39,6 +57,21 @@ class Vampire {
 
   }
 }
+
+const Original = new Vampire("Original", 1857);
+const Bart = new Vampire("Bart", 1875);
+const Ansel = new Vampire("Ansel", 1890);
+const Elgort = new Vampire("Elgort", 1901);
+const Sarah = new Vampire("Sarah", 1918);
+const Andrew = new Vampire("Andrew", 1942);
+
+Original.addOffspring(Ansel);
+Original.addOffspring(Bart);
+Ansel.addOffspring(Elgort);
+Ansel.addOffspring(Sarah);
+Elgort.addOffspring(Andrew);
+
+
 
 module.exports = Vampire;
 
